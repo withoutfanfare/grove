18cb55dc012dd4a7f9f3ff19698d263f4664e006
Danny Harding <danny@stuntrocket.co>
2026-07-04 22:43:41 +0100

    fix: remove dead beta release channel setting

--- src/stores/index.ts
+++ src/stores/index.ts
@@ -1,10 +1,10 @@
 // Pinia stores
 export { useWorktreeStore } from './worktrees';
-export { useSettingsStore, EDITOR_OPTIONS, TERMINAL_OPTIONS, GIT_CLIENT_OPTIONS, RELEASE_CHANNEL_OPTIONS } from './settings';
+export { useSettingsStore, EDITOR_OPTIONS, TERMINAL_OPTIONS, GIT_CLIENT_OPTIONS } from './settings';
 export { useRepoConfigStore } from './repoConfig';
 export { useHooksStore } from './hooks';
 export { useServicesStore } from './services';
 export { useTemplateStore, BUILTIN_TEMPLATES } from './templates';
-export type { EditorChoice, TerminalChoice, GitClientChoice, ReleaseChannel, Settings } from './settings';
+export type { EditorChoice, TerminalChoice, GitClientChoice, Settings } from './settings';
 export { useOverviewStore, formatBytes, EXPENSIVE_REFRESH_INTERVAL_MS } from './overview';
 export type { RepoSnapshot, AttentionWorktreeItem, AttentionHealthItem, AttentionRepoError, OverviewStats } from './overview';
